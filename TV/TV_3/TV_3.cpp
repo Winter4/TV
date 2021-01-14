76eba5cbe06f2c4a690050566d7598bdf470ed5e
Winter4 <Crowl1ngNice@yandex.ru>
2021-01-14 04:11:41 +0200

    3. Dunno not work

--- TV/TV_3/TV_3.cpp
+++ TV/TV_3/TV_3.cpp
@@ -1,94 +1,113 @@
 ﻿#include <iostream>
 #include "math.h"
 #include "windows.h"
+#include <vector>
 
-#define IN 0.
-#define OUT 5. 
-#define L 2.
+#define LAMBDA 0.5
 
-using namespace std;
-
-class distrib
-{
+class Generator {
 private:
-	int n;
-	float* ver;
-	float* valueX;
+	int sampleSize; // выборка
+
+	std::vector<float> intervals; // интервалы
+	std::vector<int> joinIntervals; // вхождение в интервалы
+	std::vector<float> x_values; // значения иксов
+
+	float difference;
+
 public:
-	distrib()
+	Generator() 
 	{
-		n = 100;
-		ver = new float[n]();
-		valueX = new float[n]();
-		for (int i = 0; i < n; i++)
-		{
-			valueX[i] = (OUT - IN) * (i + 1) / n;
-			ver[i] = func(valueX[i]);
-		}
-	}
-	distrib(int num)
-	{
-		if (num > 0) n = num;
-		else n = 100;
-		ver = new float[n]();
-		valueX = new float[n]();
-		for (int i = 0; i < n; i++)
-		{
-			valueX[i] = (OUT - IN) * (i + 1) / n;
-			ver[i] = func(valueX[i]);
-		}
-	}
-	~distrib()
-	{
-		delete[] ver;
-		delete[] valueX;
-	}
-	float func(float x)
-	{
-		if (x < 0) return 0;
-		return (1 - expf(-L * x));
-	}
-	int roll()
-	{
-		float random = (float)rand() * (ver[n - 1]) / RAND_MAX;
-		for (int i = 0; i < n; i++)
-			if (random <= ver[i])return i;
-	}
-	void autoroll()
-	{
-		int k;
+		// начальное и конечное значение икса
+		int x_start = -3,
+			 x_end = 3;
+		difference = (float)(x_end - x_start) / 100; // шаг икса; 50 - кол-во интервалов
+		
+		float sum1 = 0, sum2 = 0;
 
-		do
-		{
-			cout << "Размер выборки: ";
-			cin >> k;
-		} while (k < 1);
-		int* g = new int[n]();
-		for (int i = 0; i < k; i++)
-			g[roll()]++;
-
-		float hi = 0, verlast = 0, mat_oz = 0, dispersion = 0;
-		for (int i = 0; i < n; i++)
-		{
-			float pr_ver = (float)g[i] / k;
-			hi += powf(ver[i] - verlast - pr_ver, 2.) / (ver[i] - verlast);
-			dispersion += powf(valueX[i], 2) * pr_ver;
-			mat_oz += valueX[i] * pr_ver;
-			verlast = ver[i];
+		// заполняем значения иксов
+		for (float x = x_start; x <= x_end + difference; x += difference) {
+			x_values.push_back(x);
+			sum1 += Function(x);
 		}
-		hi *= k;
-		dispersion -= powf(mat_oz, 2.);
-		cout << "Математическое ожидание: " << mat_oz << "\nДисперсия: " << dispersion << "\nСреднеквадратическое отклонение:" << sqrt(dispersion) << "\nКритерий Пирсона: " << hi;
-		_getch();
-		delete[]g;
+		
+		// заполняем значния интервалов
+		for (int i = 0; i < 100; i++) {
+			sum2 += Function(x_values[i]);
+			intervals.push_back(sum2);
+
+			joinIntervals.push_back(0);
+		}
+		
+	}
+
+	float Function(float x) { // функция плотности распределения
+		//return exp(-(x * x) / (2 * 0.25) / (sqrt(2 * 3.14) * 0.5));
+		return exp(-((x * x) / 2)) / sqrt(2 * 3.14);
+	}
+
+	void PrintIntervals()
+	{
+		std::cout << "Intervals: " << std::endl;
+		for (int i : intervals)
+			std::cout << intervals[i] << std::endl;
+		std::cout << std::endl;
+	}
+
+	void MyTask()
+	{
+		std::cout << "  Enter the sample size: ";
+		// ввод выборки
+		do {
+			if (!(std::cin >> sampleSize)) {
+				std::cout << "Please enter correct number (>100): " << std::endl;
+				std::cin.clear();
+				std::cin.ignore(10000, '\n');
+			}
+		} while (sampleSize < 100);
+		
+		// проверка на попадание в интервал
+		for (int i = 0; i < sampleSize; i++) {
+			float random = (float)rand() / RAND_MAX;
+			
+			for (int j : intervals) {
+				if (intervals[j] > random) {
+					++joinIntervals[j];
+					break;
+				}
+			}
+		}
+		
+		std::cout << "X\tJoin\tMath Exp.\tDispersion\tAvg. Sqr." << std::endl;
+
+		float 
+			empiricProbability = 0,
+			mathExpectation = 0,
+			mathExpectationSquare = 0,
+			dispersion = 0,
+			kolmogorova = 0;
+
+		for (int i : intervals) {
+			empiricProbability = joinIntervals[i] / sampleSize;
+			mathExpectation += x_values[i] * empiricProbability;
+			mathExpectationSquare += (x_values[i] * x_values[i]) * empiricProbability;
+			dispersion += mathExpectationSquare - mathExpectation * mathExpectation;
+			kolmogorova += fabs(empiricProbability - intervals[i]) / sqrt(sampleSize);
+
+			std::cout << x_values[i] << "\t" << joinIntervals[i] << "\t" << mathExpectation << "\t\t" << dispersion << "\t\t" << sqrt(dispersion) << std::endl;
+		}
 	}
 };
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	distrib a;
-	a.autoroll();
+	
+	Generator lab3;
+	
+	lab3.PrintIntervals();
+	
+	lab3.MyTask();
+	
+	system("pause");
 	return 0;
 }
